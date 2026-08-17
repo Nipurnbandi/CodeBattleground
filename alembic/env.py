5af9d29abb355feca2476a90ca687ab1ba53c784
Nipurn <nipurnbandi30@gmail.com>
2026-08-18 01:06:42 +0530

    Add problem models and database migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,10 @@
 from logging.config import fileConfig
-
+from app.core.database import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from app.models.problems import Base,Problem,DefaultCodes
+
+
 
 from alembic import context
 
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata=Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- app/models/problems.py
+++ app/models/problems.py
@@ -1,18 +1,34 @@
 from app.core.database import Base
-from sqlalchemy import Column,Integer,String,Boolean,ForeignKey,DateTime,Text
+from sqlalchemy import Column,Integer,String,ForeignKey,Text,UniqueConstraint
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
-from typing import Optional
 from sqlalchemy.orm import relationship
 
 
-class Problems(Base):
-    __tablename__="problems"
+class Problem(Base):
+    __tablename__="problem"
+
     id=Column(Integer,primary_key=True,nullable=False)
     slug=Column(String(100),nullable=False,unique=True)
     title=Column(String(250),nullable=False)
     statement_markdown=Column(Text,nullable=False)
-    points=Column(Integer, nullable=False)
+    points=Column(Integer,nullable=False)
     time_limit_seconds=Column(Integer,nullable=False)
     memory_limit_mb=Column(Integer,nullable=False)
-    created_at=Column(TIMESTAMP,server_default=text('now()'),nullable=False)
+    created_at=Column(TIMESTAMP,server_default=text("now()"),nullable=False)
+
+    default_codes=relationship("DefaultCodes",back_populates="problem")
+
+
+class DefaultCodes(Base):
+    __tablename__="default_codes"
+
+    __table_args__=(UniqueConstraint("problem_id","language"),)
+
+    id=Column(Integer,primary_key=True,nullable=False)
+    problem_id=Column(Integer,ForeignKey("problem.id",ondelete="CASCADE"),nullable=False)
+    language=Column(String(10),nullable=False)
+    starter_code=Column(Text,nullable=False)
+    full_boilerplate=Column(Text,nullable=False)
+
+    problem=relationship("Problem",back_populates="default_codes")
